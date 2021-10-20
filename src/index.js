796f416cd5bce74431000b3b73f694ccd5ba1ce9
hiraku333 <62025844+hiraku333@users.noreply.github.com>
2021-10-20 23:34:44 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,33 @@
+// var vall = "var変数";
+// console.log(vall);
+
+// vall = "var変数を上書き";
+// console.log(vall);
+
+// var vall = "var変数を再宣言";
+// console.log(vall);
+
+// let val2 = "let変数";
+// console.log(val2);
+// val2 =  "let変数を上書き可能";
+// console.log(val2);
+
+// let val2 = "再宣言";
+
+// const val3= "const変数";
+// console.log(val3);
+// val3 = "const変数を上書き";
+
+// const val3 = "const再宣言";
+
+const val4 = {
+  name: "ともひろ",
+  age: 27
+};
+val4.name = "ひろと";
+val4.adress = "hamamatsu";
+console.log(val4);
+
+const val5 = ["dog", "cat"];
+val5.push("monkey");
+console.log(val5);
